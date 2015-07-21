8354c78bd1242985871084613ac297601b592fba
iAndyDev <perepamanohar@gmail.com>
2015-07-21 22:29:35 +0530

    Added Materail Progress & GCM Methods

---
+++ src/com/bookmyskills/GCMIntentService.java
@@ -0,0 +1,123 @@
+/*
+ * Copyright 2012 Google Inc. Licensed under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law
+ * or agreed to in writing, software distributed under the License is
+ * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+package com.bookmyskills;
+
+import static com.google.android.gcm.CommonUtilities.SENDER_ID;
+
+import java.util.Iterator;
+import java.util.Random;
+import java.util.Set;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.AudioManager;
+import android.os.Bundle;
+import android.os.PowerManager;
+import android.os.PowerManager.WakeLock;
+import android.support.v4.app.NotificationCompat;
+import android.text.TextUtils;
+import android.util.Log;
+
+import com.google.android.gcm.CommonUtilities;
+import com.google.android.gcm.GCMBaseIntentService;
+import com.google.android.gcm.GCMRegistrar;
+import com.google.android.gcm.ServerUtilities;
+import com.utils.StorageClass;
+
+/**
+ * IntentService responsible for handling GCM messages.
+ */
+public class GCMIntentService extends GCMBaseIntentService {
+
+	private static final String TAG = "GCMIntentService";
+	private int startId;
+
+	// static JSONObject mJsonObject;
+
+	public GCMIntentService() {
+		super(SENDER_ID);
+	}
+
+	@Override
+	protected void onRegistered(Context context, String registrationId) {
+		Log.i(TAG, "Device registered: regId = " + registrationId);
+
+		StorageClass userUtils = StorageClass.getInstance(context);
+		userUtils.setDeviceToken(registrationId);
+		ServerUtilities.register(context, registrationId);
+	}
+
+	@Override
+	protected void onUnregistered(Context context, String registrationId) {
+		Log.i(TAG, "Device unregistered");
+		if (GCMRegistrar.isRegisteredOnServer(context)) {
+			ServerUtilities.unregister(context, registrationId);
+		} else {
+			// This callback results from the call to unregister made on
+			// ServerUtilities when the registration to the server failed.
+			Log.i(TAG, "Ignoring unregister callback");
+		}
+	}
+
+	@Override
+	public void onStart(Intent intent, int startId) {
+		this.startId = startId;
+		// TODO Auto-generated method stub
+		super.onStart(intent, startId);
+	}
+
+	@Override
+	protected void onMessage(final Context context, final Intent intent) {
+	}
+
+	public JSONObject getotherparam(Bundle mPushBundle) {
+		JSONObject jsonObject = new JSONObject();
+		if (mPushBundle != null) {
+			Set<String> set = mPushBundle.keySet();
+			Iterator<String> iterator = set.iterator();
+			while (iterator.hasNext()) {
+				String key = iterator.next();
+				Object value = mPushBundle.get(key);
+				try {
+					jsonObject.put(key, value);
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return jsonObject;
+	}
+
+	@Override
+	protected void onDeletedMessages(Context context, int total) {
+		Log.i(TAG, "Received deleted messages notification");
+
+	}
+
+	@Override
+	public void onError(Context context, String errorId) {
+		Log.i(TAG, "Received error: " + errorId);
+	}
+
+	@Override
+	protected boolean onRecoverableError(Context context, String errorId) {
+		Log.i(TAG, "Received recoverable error: " + errorId);
+		return super.onRecoverableError(context, errorId);
+	}
+
+}
